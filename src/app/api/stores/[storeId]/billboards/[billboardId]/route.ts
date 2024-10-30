d1a714bb97c2ca94a461968355d44c515abfc084
Mohammad <bhshtbaran@gmail.com>
2024-10-30 07:32:49 -0700

    fear: implement api to update billboard

---
+++ src/app/api/stores/[storeId]/billboards/[billboardId]/route.ts
@@ -0,0 +1,64 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+import prisma from "@/lib/db";
+
+export const PATCH = async (
+  request: NextRequest,
+  params: { params: { storeId: string; billboardId: string } }
+) => {
+  try {
+    const body = await request.json();
+    const { params: sendParams } = params;
+    const { userId } = auth();
+
+    if (!userId) {
+      return NextResponse.json({ message: "UnAuthorized" }, { status: 401 });
+    }
+
+    if (!sendParams.billboardId) {
+      return NextResponse.json(
+        { message: "BillboardId is required." },
+        { status: 400 }
+      );
+    }
+
+    if (!sendParams.storeId) {
+      return NextResponse.json(
+        { message: "storeId is required." },
+        { status: 400 }
+      );
+    }
+
+    const billboard = await prisma.billboard.findUnique({
+      where: { id: sendParams.billboardId, storeId: sendParams.storeId },
+    });
+
+    if (!billboard) {
+      return NextResponse.json(
+        { message: "Invalid billboard" },
+        { status: 404 }
+      );
+    }
+
+    const updatedBillboard = await prisma.billboard.update({
+      where: {
+        id: sendParams.billboardId,
+        storeId: sendParams.storeId,
+      },
+      data: {
+        label: body.title,
+        imageUrl: body.imageUrl,
+      },
+    });
+
+    return NextResponse.json(updatedBillboard);
+  } catch (error) {
+    console.error("[BILLBOARD_ERROR]", error);
+
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
