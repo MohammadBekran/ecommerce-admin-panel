4818cc4e1ed311c3c78ef301abfb5277ec0af74d
Mohammad <bhshtbaran@gmail.com>
2024-10-30 12:21:54 -0700

    feat: implement api to create a category

---
+++ src/app/api/stores/[storeId]/categories/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { createCategorySchema } from "@/features/categories/core/validations";
+
+import prisma from "@/lib/db";
+
+export const POST = async (
+  request: NextRequest,
+  params: { params: { storeId: string } }
+) => {
+  try {
+    const body = await request.json();
+
+    const { name, billboardId } = body;
+    const { params: sendParams } = params;
+
+    const validation = createCategorySchema.safeParse(body);
+
+    if (!validation.success)
+      return NextResponse.json(validation.error.format(), { status: 400 });
+
+    const newStore = await prisma.category.create({
+      data: {
+        storeId: sendParams.storeId,
+        name,
+        billboardId,
+      },
+    });
+
+    return NextResponse.json(newStore, { status: 201 });
+  } catch (error) {
+    console.error("[CATEGORY_ERROR]", error);
+
+    return NextResponse.json("Internal server error", { status: 500 });
+  }
+};
