f1dc99691520de0bacf56942896f454ec30dc69e
Mohammad <bhshtbaran@gmail.com>
2024-11-04 23:35:25 -0800

    feat: implement API for get categories

--- src/app/api/stores/[storeId]/categories/route.ts
+++ src/app/api/stores/[storeId]/categories/route.ts
@@ -4,6 +4,32 @@ import { createCategorySchema } from "@/features/categories/core/validations";
 
 import prisma from "@/lib/db";
 
+export const GET = async (
+  request: NextRequest,
+  params: { params: { storeId: string } }
+) => {
+  try {
+    const { params: sendParams } = params;
+
+    const categories = await prisma.category.findMany({
+      where: {
+        storeId: sendParams.storeId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    console.log(categories);
+
+    return NextResponse.json(categories);
+  } catch (error) {
+    console.error("[CATEGORY_ERROR]", error);
+
+    return NextResponse.json("Internal server error", { status: 500 });
+  }
+};
+
 export const POST = async (
   request: NextRequest,
   params: { params: { storeId: string } }
