f738be53f864316abd8ff788b8d2ce5a0365e5c2
Mohammad <bhshtbaran@gmail.com>
2024-11-05 20:34:22 -0800

    feat: implement API to delete store

--- src/app/api/stores/[storeId]/route.ts
+++ src/app/api/stores/[storeId]/route.ts
@@ -42,3 +42,38 @@ export const PATCH = async (
     return NextResponse.json("Internal server error", { status: 500 });
   }
 };
+
+export const DELETE = async (
+  request: NextRequest,
+  params: { params: { storeId: string } }
+) => {
+  try {
+    const { userId } = auth();
+
+    if (!userId)
+      return NextResponse.json({ message: "UnAuthorized" }, { status: 401 });
+
+    const { params: sendParams } = params;
+
+    const where = { id: sendParams.storeId };
+
+    const store = await prisma.store.findUnique({
+      where,
+    });
+
+    if (!store)
+      return NextResponse.json({ message: "Invalid store" }, { status: 404 });
+
+    const deletedStore = await prisma.store.delete({
+      where,
+    });
+
+    return NextResponse.json(deletedStore);
+  } catch (error) {
+    console.error("[STORE_ERROR]", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
